779bec58fc5dcd74a3f2ead28167390a2f2f3af6
blinea <blineaa@gmail.com>
2022-05-29 02:11:54 -0400

    Login DB

--- app/src/main/java/com/greenhouse/greenhouseapp/activity/LoginActivity.java
+++ app/src/main/java/com/greenhouse/greenhouseapp/activity/LoginActivity.java
@@ -10,20 +10,39 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.greenhouse.greenhouseapp.R;
 import com.greenhouse.greenhouseapp.controller.SessionManagement;
 import com.greenhouse.greenhouseapp.model.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText etEmail, etPassword;
     Button btnRegister, btnLogIn;
     String email, password;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            email = extras.getString("email");
+            password = extras.getString("password");
+        }
 
         initUI();
 
@@ -53,7 +72,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         if (id == R.id.btnLogin) {
 
-            login();
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            login(email, password);
 
         } else if (id == R.id.btnRegister) {
 
@@ -86,16 +108,51 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    public void login() {
+    public void login(String email, String password) {
+        String URLDB = "http://192.168.0.3/greenhousedb/login.php?email=" + email + "&password=" + password;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URLDB,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String email, password;
+                        try{
 
-        User user = new User(666, "Satan", "sa@tan.com", "lol", "hell");
 
-        SessionManagement sessionManagement =  new SessionManagement(LoginActivity.this);
-        sessionManagement.saveSession(user);
+                            //----------------
 
-        Toast.makeText(LoginActivity.this, user.toString(), Toast.LENGTH_SHORT).show();
+                            email = response.getString("email");
+                            password = response.getString("password");
 
-        sendToMenu();
+                            User user = new User(666, "Satan", email, password, "hell");
+
+                            SessionManagement sessionManagement =  new SessionManagement(LoginActivity.this);
+                            sessionManagement.saveSession(user);
+
+                            etEmail.setText(email);
+                            etPassword.setText(password);
+
+                            Toast.makeText(LoginActivity.this, user.toString(), Toast.LENGTH_SHORT).show();
+
+                            sendToMenu();
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
 
     }
 
