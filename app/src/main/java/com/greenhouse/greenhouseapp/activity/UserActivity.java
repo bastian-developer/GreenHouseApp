0d1d100ca7f84f99424cb36b2de98fd880c0ae92
blinea <blineaa@gmail.com>
2022-06-01 20:28:26 -0400

    Edit Profile

--- app/src/main/java/com/greenhouse/greenhouseapp/activity/UserActivity.java
+++ app/src/main/java/com/greenhouse/greenhouseapp/activity/UserActivity.java
@@ -1,23 +1,30 @@
 package com.greenhouse.greenhouseapp.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.greenhouse.greenhouseapp.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -65,17 +72,48 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
             String name = etName.getText().toString().trim();
             String email = etEmail.getText().toString().trim();
             String password = etPassword.getText().toString().trim();
-            String address = etPhoto.getText().toString().trim();
+            String photo = etPhoto.getText().toString().trim();
 
-            editProfile();
+            editProfile(name, email, password, photo);
+            sendToMenu();
 
         }
 
     }
 
-    private void editProfile() {
-        Toast.makeText(UserActivity.this, "xd", Toast.LENGTH_SHORT).show();
+    private void editProfile(final String name, final String email, final String password, final String photo) {
+        String URLDB = "http://192.168.0.3/greenhousedb/editUser.php";
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                URLDB,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        finish();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
+                    }
+                }
+        ){
+            //nullable
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("id", userID);
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                params.put("photo", photo);
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
     }
 
     private void searchUser() {
@@ -115,5 +153,14 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         requestQueue.add(jsonObjectRequest);
     }
 
+    public void sendToMenu() {
+        Intent i = new Intent(UserActivity.this, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        Bundle sendData = new Bundle();
+        sendData.putString("id", String.valueOf(userID));
+        i.putExtras(sendData);
+        startActivity(i);
+    }
 
 }
